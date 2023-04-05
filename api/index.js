de15ee3319bf6199ee93e477d1a11a8172178745
lucas salvatierra <lucassnahuel1759@gmail.com>
2023-04-05 19:37:19 -0300

    d

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const {PORT} = process.env;
+const {PORT} = process.env || 3001;
 const getAllTemperaments = require("./src/controllers/getAllTemperaments")
 // Syncing all the models at once.
 conn.sync({ alter: true }).then( async() => {
